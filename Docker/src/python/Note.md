9330208886fddaf640855e34a98022f93d359aa9
dmarinova1 <francedkm@yahoo.fr>
2020-06-15 09:18:02 +0200

    moved folders

---
+++ Docker/src/python/Note.md
@@ -0,0 +1,1 @@
+Example inspired by [Red Hat Developper: How to build your "Hello World" Container Using Python](https://developers.redhat.com/articles/csharp-container/)
--- Docker/src/python/helloworld.py
+++ Docker/src/python/helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return 'Hello Luxembourg!'
+        return 'Hello World!'
 
 api.add_resource(Greeting, '/') # Route_1
 
